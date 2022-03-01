154c12b2e265232e9c18a85c769f5552342bf20f
sardaceylan <arda_ceylanrnwr@yahoo.com>
2022-03-01 07:54:08 +0300

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require("express")
+const bodyParser = require("body-parser")
+const { check, validationResult } = require("express-validator")
 
-app.set("view engine", "pug");
-app.set("views");
+const app = express()
 
-const routes = require("./routes/route");
+app.set("view engine", "ejs")
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+const urlencoded = bodyParser.urlencoded({ extended: false })
 
-app.use(routes);
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.post("/register", urlencoded, (req, res) => {
+    // res.json(req.body)
+    console.log(req.body);
+})
 
 app.listen(3000, () => {
     console.log("Hello World!");
--- routes/route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const path = require("path");
-
-const main = require("../controllers/main-controller");
-
-router.get("/", main.getForm);
-router.post("/api", main.postForm);
-module.exports = router;
